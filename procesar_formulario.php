37507e78c9827b31dfdb15902d073701b9ac0232
jeanvg777 <ja0795435@gmail.com>
2024-08-16 19:35:54 -0500

    Update procesar_formulario.php

--- procesar_formulario.php
+++ procesar_formulario.php
@@ -3,12 +3,12 @@
 include 'conexion.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $Nombre = $_POST['Nombre'];
-    $Documento = $_POST['Documento'];
-    $Fecha_de_Nacimiento = $_POST['Fecha_de_Nacimiento'];
-    $Email = $_POST['Email'];
-    $Direccion = $_POST['Direccion'];
-    $Telefono = $_POST['Telefono'];
+    $Nombre = htmlspecialchars($_POST['Nombre']);
+    $Documento = htmlspecialchars($_POST['Documento']);
+    $Fecha_de_Nacimiento = htmlspecialchars($_POST['Fecha_de_Nacimiento']);
+    $Email = htmlspecialchars($_POST['Email']);
+    $Direccion = htmlspecialchars($_POST['Direccion']);
+    $Telefono = htmlspecialchars($_POST['Telefono']);
 
     $stmt = $conn->prepare("INSERT INTO estudiantes (Nombre, Documento, Fecha_de_Nacimiento, Email, Direccion, Telefono) VALUES (?, ?, ?, ?, ?, ?)");
     $stmt->bind_param("ssssss", $Nombre, $Documento, $Fecha_de_Nacimiento, $Email, $Direccion, $Telefono);
@@ -19,7 +19,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Error: " . $stmt->error;
     }
 
-    
     $stmt->close();
     $conn->close();
 }
